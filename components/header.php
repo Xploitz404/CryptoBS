c2a7ca9e7871c47ba0f30389b5d8604265036db2
Oscar Lara <me@oscarlara.dev>
2020-11-24 15:28:54 -0600

    Se agregan estilos iniciales

---
+++ components/header.php
@@ -0,0 +1,12 @@
+<nav class="header">
+    <figure class="logo">
+        <img src="/assets/CryptoBS_isologo.svg" alt="Logotipo de CryptoBS">
+        <div class="blank"></div>
+    </figure>
+    <ul class="links">
+        <li class="link">Blog</li>
+        <li class="link">Aprende</li>
+        <li class="link">Iniciar sesión</li>
+        <li class="link">Crear Cuenta</li>
+    </ul>
+</nav>
---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CryptoBS</title>
+    <link rel="stylesheet" href="./styles/main.css">
+    <link rel="stylesheet" href="./styles/buttons.css">
+    <link rel="stylesheet" href="./styles/input.css">
+    <link rel="stylesheet" href="./components/header.css">
+</head>
+<body>
+    <?php require_once "./components/header.php" ?>
+    <p class="title">Título</p>
+    <p class="paragraph">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime explicabo consequatur iste rem illo at id doloremque officiis quos possimus molestias voluptatum voluptas quasi itaque, dicta perferendis impedit nulla? Doloribus?
+    </p>
+    <button class="button primary">Enviar!</button><br>
+    <input type="text" class="input-active" placeholder="Nombre(s)">
+    <input type="text" class="input-disabled" placeholder="Nombre(s)">
+</body>
+</html>
