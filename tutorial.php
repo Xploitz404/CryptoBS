555cb26605475213028b404c2a0f45d596f6dcae
Oscar Lara <me@oscarlara.dev>
2020-11-25 14:47:21 -0600

    Se agrega tutorial

---
+++ tutorial.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html
+  PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>CryptoBS</title>
+  <link rel="stylesheet" href="./styles/main.css">
+  <link rel="stylesheet" href="./styles/buttons.css">
+  <link rel="stylesheet" href="./styles/input.css">
+  <link rel="stylesheet" href="./components/header.css">
+  <style>
+    table {
+      margin: 0 auto;
+      font-family: var(--main-font);
+    }
+  </style>
+</head>
+
+<body>
+  <?php require_once "./components/header.php" ?>
+  <table width="1114" height="584" border="0" bgcolor="#EFFAD3">
+    <tr align="center">
+      <td height="91" colspan="6"><span class="Estilo1">Conoce el uso de las criptomonedas mediante el manejo de una
+          simulacion de compra/venta de las mismas</span></td>
+    </tr>
+    <tr align="center">
+      <td width="124" height="45">&nbsp;</td>
+      <td width="225"><span class="Estilo4 Estilo1">1</span></td>
+      <td width="119">&nbsp;</td>
+      <td width="227"><span class="Estilo4 Estilo1">2</span></td>
+      <td width="131">&nbsp;</td>
+      <td width="262"><span class="Estilo1">3</span></td>
+    </tr>
+    <tr align="center">
+      <td height="180" colspan="2"><img src="./assets/img/1.png" width="243" height="152" /></td>
+      <td colspan="2"><img src="./assets/img/2.png" width="233" height="157" /></td>
+      <td colspan="2"><img src="./assets/img/3.png" width="248" height="164" /></td>
+    </tr>
+    <tr align="center">
+      <td height="37">&nbsp;</td>
+      <td><span class="Estilo1">4</span></td>
+      <td>&nbsp;</td>
+      <td><span class="Estilo1">5</span></td>
+      <td>&nbsp;</td>
+      <td><span class="Estilo1">6</span></td>
+    </tr>
+    <tr align="center">
+      <td colspan="2"><img src="./assets/img/4.png" width="235" height="143" /></td>
+      <td colspan="2"><img src="./assets/img/5.png" width="238" height="152" /></td>
+      <td colspan="2"><img src="./assets/img/6.png" width="247" height="165" /></td>
+    </tr>
+  </table>
+
+
+</body>
+
+</html>
